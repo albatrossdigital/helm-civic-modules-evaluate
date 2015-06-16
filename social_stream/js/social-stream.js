ec0714a97c3816f5119bc081adfd6bc23c88ce15
Jeff Lyon <jeff@albatrossdigital.com>
2015-06-16 11:41:27 -0700

    Working on social stream insta

--- social_stream/js/social-stream.js
+++ social_stream/js/social-stream.js
@@ -3,7 +3,6 @@
 Drupal.behaviors.social_stream = {
   attach: function (context, settings) {
     settings.social_stream.days = 100;
-    console.log(settings.social_stream);
     $('#social-stream').dcSocialStream(settings.social_stream);
 
   }
