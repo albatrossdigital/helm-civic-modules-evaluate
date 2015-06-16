7f42cd9c6bf22a6ba3f246534fdc9f75e35eee82
Jeff Lyon <jeff@albatrossdigital.com>
2015-06-16 12:07:03 -0700

    Set limit to social stream

--- social_stream/js/social-stream.js
+++ social_stream/js/social-stream.js
@@ -2,7 +2,9 @@
 
 Drupal.behaviors.social_stream = {
   attach: function (context, settings) {
-    settings.social_stream.days = 100;
+    settings.social_stream.days = 30;
+    settings.social_stream.limit = 10;
+    settings.social_stream.max = 'limit';
     $('#social-stream').dcSocialStream(settings.social_stream);
 
   }
